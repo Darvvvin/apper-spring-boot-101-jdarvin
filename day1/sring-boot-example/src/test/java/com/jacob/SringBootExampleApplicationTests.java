095a4394e34e38a27faf7c4cc85495531c6a091c
Jacob Darvin <jacobisdarvin@gmail.com>
2023-06-21 13:54:23 +0800

    self study

---
+++ day1/sring-boot-example/src/test/java/com/jacob/SringBootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SringBootExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
