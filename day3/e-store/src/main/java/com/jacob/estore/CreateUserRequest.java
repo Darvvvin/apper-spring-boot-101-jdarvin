f1599d6c9ddbb9bdedf7c17ed9dcaa6972a16562
Jacob Darvin <jacobisdarvin@gmail.com>
2023-07-04 11:14:19 +0800

    Update CreateUserRequest.java
    Added extra validation

--- day3/e-store/src/main/java/com/jacob/estore/CreateUserRequest.java
+++ day3/e-store/src/main/java/com/jacob/estore/CreateUserRequest.java
@@ -1,26 +1,35 @@
 package com.jacob.estore;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CreateUserRequest {
+    @NotBlank(message = "`email` is required")
+    @Email(message = "invalid email format")
     private String email;
+
+    @NotBlank(message = "password is required")
+    @Size(min = 8, message = "password must be at least 8 char")
     private String password;
 
     @JsonProperty("first_name")
-    @NotBlank
+    @NotBlank(message = "first_name is required")
     private String firstName;
 
     @JsonProperty("middle_name")
     private String middleName;
 
     @JsonProperty("last_name")
-    @NotBlank
+    @NotBlank(message = "last_name is required")
     private String lastName;
 
     @JsonProperty("birth_date")
-    @NotBlank
+    @NotBlank(message = "birth_date is required")
+    @Pattern(regexp = "[0-9]{4}-[0-9]{2}-[0-9]{2}")
     private String birthDate;
 }
