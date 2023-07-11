881c67feda8710511fc1ae7970973bf0ef4773e6
Jacob Darvin <jacobisdarvin@gmail.com>
2023-07-11 10:57:38 +0800

    Added service and blogger

--- day4/the-blog-service/src/main/java/com/apper/theblogservice/model/Blogger.java
+++ day4/the-blog-service/src/main/java/com/apper/theblogservice/model/Blogger.java
@@ -1,8 +1,13 @@
 package com.apper.theblogservice.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
 import lombok.Data;
-import org.springframework.cglib.core.Local;
 
 import java.time.LocalDateTime;
 
@@ -10,6 +15,7 @@ import java.time.LocalDateTime;
 @Table(name = "BLOGGER")
 @Data
 public class Blogger {
+
     @Id
     @Column(name = "ID")
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -31,4 +37,5 @@ public class Blogger {
     public void setCreatedAt() {
         createdAt = LocalDateTime.now();
     }
+
 }
