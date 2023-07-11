4b904d13fc88328874e5edb4f61c40134c33a9dd
Jacob Darvin <jacobisdarvin@gmail.com>
2023-07-11 09:36:44 +0800

    Initial commit of day4 project

---
+++ day4/the-blog-service/src/main/java/com/apper/theblogservice/TheBlogServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.apper.theblogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBlogServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheBlogServiceApiApplication.class, args);
+	}
+
+}
